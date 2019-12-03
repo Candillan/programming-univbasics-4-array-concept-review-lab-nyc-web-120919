135f6c73eb819a87b8ff31b144e1f27f4998c62d
careful-principle-0303 <cabaker96@aol.com>
2019-12-03 22:17:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,10 +13,24 @@ def find_max_value(array)
   # Add your solution here
   counter = 0
   while counter < array.length do
-    
+    if !max
+      max = array[counter]
+    elsif array[counter] > max
+      max = array[counter]
+    end
+    counter += 1
   end
+  return max
 end
 
 def find_min_value(array)
   # Add your solution here
+  counter = 0 
+  while counter < array.length do
+    if !min 
+      min = array[counter]
+      elsif
+      
+      
+  end
 end
